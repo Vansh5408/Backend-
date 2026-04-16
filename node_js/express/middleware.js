c03adb55ed063fa75352eb7f5fae3546a98640d3
Vansh Jindal <vansh.jindal1450@gmail.com>
2026-04-16 20:16:38 +0530

    error handling in express

--- node_js/express/middleware.js
+++ node_js/express/middleware.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app=express();
+
+// this is middleware making 
 app.use((req,res,next)=>{
     console.log("first middlewire");
     next();
@@ -7,13 +9,21 @@ app.use((req,res,next)=>{
 app.use((req,res,next)=>{
     console.log("second middlewire");
     next();
-})
+}) 
+
+// this is making routes 
 app.get('/',(req,res)=>{
     res.send("this is server first request");
 })
 
 app.get('/profile',(req,res)=>{
     res.send("this is profile window");
+})
+
+// error handling inn routes
+app.use((err, req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 })
 app.listen(3000,()=>{
     console.log("port is 3000");
